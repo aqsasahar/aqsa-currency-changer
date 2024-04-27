3efe3742872f593e3ec20c145b7d713479964ecc
aqsasahar <165950897+aqsasahar@users.noreply.github.com>
2024-04-27 19:27:00 +0500

    my first commit

---
+++ index.js
@@ -0,0 +1,34 @@
+import inquirer from "inquirer";
+// data
+const currency = {
+    'USD': 1,
+    'EUR': 0.91,
+    'GBP': 0.76,
+    'INR': 74.57,
+    'PKR': 280,
+};
+let user_Answer = await inquirer.prompt([
+    {
+        name: 'from',
+        message: 'Entter From currency',
+        type: 'list',
+        choices: ['USD', 'EUR', 'GBP', 'INR', 'PKR'],
+    },
+    {
+        name: 'To',
+        message: 'Entter To currency',
+        type: 'list',
+        choices: ['USD', 'EUR', 'GBP', 'INR', 'PKR'],
+    },
+    {
+        name: 'Amount',
+        message: 'Entter Your Amount',
+        type: 'number',
+    },
+]);
+let FromAmount = currency[user_Answer.from];
+let ToAmount = currency[user_Answer.To];
+let Amount = user_Answer.Amount;
+let baseAmount = Amount / FromAmount; // USD base currency //6
+let convertedAmount = baseAmount * ToAmount;
+console.log(convertedAmount);
